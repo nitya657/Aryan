54c5225d88214f9ac75a67e44dcaadb744532478
OWNER-OF-BOT-ARYAN <aryanbot265@gmail.com>
2025-01-10 18:00:23 +0530

    Update 0_ARYAN-PAIR6.js

--- models/commands/0_ARYAN-PAIR6.js
+++ models/commands/0_ARYAN-PAIR6.js
@@ -41,7 +41,7 @@ async function makeImage({ one, two }) {
 
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    pairing_img.composite(circleOne.resize(200, 200), 153, 148).composite(circleTwo.resize(200, 200), 510, 152);
+    pairing_img.composite(circleOne.resize(200, 200), 152, 148).composite(circleTwo.resize(200, 200), 510, 150);
 
     let raw = await pairing_img.getBufferAsync("image/png");
 
