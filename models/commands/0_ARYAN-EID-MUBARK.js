853c3d64b441e96ff17fd663cb95daa5e62e3ad4
bot-aryan-nitya <wifearyanki@gmail.com>
2025-03-30 13:13:39 +0530

    Update 0_ARYAN-EID-MUBARK.js

--- models/commands/0_ARYAN-EID-MUBARK.js
+++ models/commands/0_ARYAN-EID-MUBARK.js
@@ -22,7 +22,7 @@ react.includes("🤲")) {
         body: `${name} 💐𝐀𝐀𝐏 𝐒𝐀𝐁 𝐊𝐎 𝐀𝐔𝐑 𝐀𝐏𝐊𝐈 𝐅𝐄𝐌𝐋𝐘 𝐊𝐎 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐀𝐑𝐘𝐀𝐍 𝐊𝐈 𝐓𝐀𝐑𝐀𝐅 𝐒𝐄 𝐇𝐀𝐏𝐘 𝐄𝐈𝐃 𝐌𝐔𝐁𝐀𝐑𝐀𝐊💐✿`,attachment: fs.createReadStream(__dirname + `/Aryan/received_1319184765957065.mp4`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("😋", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🤲", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
