9577590c4d006f7f2dd824102cf2dcba1b58bba7
motu-bot662 <birjeshkumarverma9628@gmail.com>
2025-07-10 09:47:23 +0530

    Update 0_linkautodownload.js

--- models/commands/0_linkautodownload.js
+++ models/commands/0_linkautodownload.js
@@ -17,7 +17,7 @@ module.exports = {
     const fs = require("fs-extra");
     const content = event.body ? event.body : "";
     const body = content.toLowerCase();
-    const { alldown } = require("arif-babu-media");
+    const { alldown } = require("aryan-ig-downloader");
     if (body.startsWith("https://")) {
       api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
       const data = await alldown(content);
