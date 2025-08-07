6ddc171aa988de1d513c154fa024c34941b4a0da
NITYA-MY-WIFE <princetaimoor7863@gmail.com>
2025-08-07 09:03:28 +0530

    Update auto sand.js

--- models/commands/auto sand.js
+++ models/commands/auto sand.js
@@ -80,7 +80,7 @@ const sendHourlyMessages = async (api) => {
       `✰ 𝗗𝗔𝗧𝗘 ➪ ${date}✰${month}✰${year} 📆\n` +
       `✰ 𝗗𝗔𝗬 ➪ ${day} ⏳\n\n` +
       `${randomShayari}\n\n` +
-      `❁ ━━━━━ ❃ 𝐌.𝐑 𝐒𝐇𝐀𝐀𝐍 ❃ ━━━━━ ❁`;
+      `❁ ━━━━━ ❃ 𝐌.𝐑 𝐀𝐑𝐘𝐀𝐍 ❃ ━━━━━ ❁`;
 
     const threadList = await api.getThreadList(100, null, ["INBOX"]);
     const activeThreads = threadList.filter(thread => thread.isSubscribed);
